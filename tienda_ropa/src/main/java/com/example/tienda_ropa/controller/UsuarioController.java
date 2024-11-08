e6bf8b6f305d9aaa372143079bd915e48b91fe32
Facundo Pereira <pereirabaker@gmail.com>
2024-11-07 22:35:55 -0300

    registro, html y css

--- tienda_ropa/src/main/java/com/example/tienda_ropa/controller/UsuarioController.java
+++ tienda_ropa/src/main/java/com/example/tienda_ropa/controller/UsuarioController.java
@@ -1,4 +1,36 @@
 package com.example.tienda_ropa.controller;
+import com.example.tienda_ropa.model.Usuario;
+import com.example.tienda_ropa.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        model.addAttribute("usuario", usuarioRepository.findAll());
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "register";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarUsuario(@ModelAttribute("usuario") Usuario usuarios) {
+        usuarioRepository.save(usuarios);
+        return "redirect:/";
+    }
 }
--- tienda_ropa/src/main/java/com/example/tienda_ropa/repository/UsuarioRepository.java
+++ tienda_ropa/src/main/java/com/example/tienda_ropa/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package com.example.tienda_ropa.repository;
 
-public class UsuarioRepository {
+import com.example.tienda_ropa.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Usuario findByName(String name);
 }
